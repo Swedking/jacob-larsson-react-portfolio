82b32398244ac0fe5c4d82ece47a127fbb5d2aa5
Jacob Larsson <iamwingman91@gmail.com>
2020-12-19 19:04:27 -0700

    introductino to state in react

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Jacob Larsson's Portfolio</h1>
+        <PortfolioContainer/>
       </div>
     );
   }
