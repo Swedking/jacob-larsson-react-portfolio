e31e86518bc5de6bc2c6b8094f3b2cdb7f2422f6
Jacob Larsson <iamwingman91@gmail.com>
2021-01-04 19:31:08 -0700

    1/4/2020 code update

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,40 +1,80 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import axios from "axios";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-constructor() {
+  constructor() {
     super();
 
     this.state = {
-        pageTitle: "Welcome to my portfolio",
-        data: [
-            {title:"Quip"}, 
-            {title:"Eventbrite"}, 
-            {title:"Ministry Safe"},
-            {title:"SwingAway"}
-            ]
-        };
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jacoblarsson.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem
+          key={item.id}
+          title={item.name}
+          url={item.url}
+          slug={item.id}
+        />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"}/>;
-        })
-    }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-    render() {
-        return(
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                {this.portfolioItems()}
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
-
-// usual git workflow
-// git add .
-// git commit -m "message"
-// git push
